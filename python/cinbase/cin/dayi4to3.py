895988e53eab491b64aaddd67910f860581c05b5
Richard Liu <richliu@techarea.org>
2019-03-24 17:09:10 +0800

    improve dayi4 to 3 choice key
    Signed-off-by: Richard Liu <richliu@techarea.org>

--- python/cinbase/cin/dayi4to3.py
+++ python/cinbase/cin/dayi4to3.py
@@ -29,6 +29,11 @@ class DAYI4TO3:
             with open(name, encoding='utf8') as f:
                 lines = [line.rstrip('\n') for line in f]
 
+
+                keylist = {}
+
+                inscount = 1
+
                 for l in lines:
                     if startconver is 0:
                         if l.find("%chardef begin"):
@@ -41,12 +46,37 @@ class DAYI4TO3:
 
                             # add 3 key first
                             if result:
-                                dkey = result.group(1)
-                                if len(dkey) is 4:
-                                    temp = dkey[0:2] + dkey[3] + " " + result.group(2)
-                                    self.buf.append(temp)
+                                dkey = result.group(1).strip()
+                                keylist[dkey] = result.group(2).strip()
 
                         self.buf.append(l)
+                
+                for n, l in enumerate(lines):
+                    if startconver is 0:
+                        if l.find("%chardef begin"):
+                            print("start convert")
+                            startconver = 1
+                    else:
+                        if l[:2] != "##":
+                            result = dayi_re.search(l)
+
+                            # add 3 key first
+                            if result:
+                                dkey = result.group(1).strip()
+                                if len(dkey) is 4:
+                                    newdkey = dkey[0:2] + dkey[3]
+                                    if newdkey not in keylist:
+                                        print("key %s : %s not in list, insert it" % (newdkey,
+                                            result.group(2).strip()))
+                                        temp = newdkey + "  " + result.group(2).strip()
+
+                                        # rarely situation one key will have two difference 4 key disassemble 
+                                        keylist[newdkey] = result.group(2).strip()
+                                        self.buf.insert(n + inscount, temp)
+                                        inscount += 1
+
+
+
             f.close()
 
         else:
@@ -56,5 +86,5 @@ class DAYI4TO3:
 if __name__ == "__main__":
 
     dayi = DAYI4TO3()
-    dayi.loaddayi("thdayi.cin")
+    dayi.loaddayi("dayi4.cin")
     dayi.write()
